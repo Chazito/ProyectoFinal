ee04b987c3e72e96d25bb7b2e62b95a2ed495995
HokeRompeorli <Hamechrs@gmail.com>
2017-11-15 20:25:03 -0300

    Clase Objeto def

--- ProyectoFinal/src/proyectofinal/Objeto.java
+++ ProyectoFinal/src/proyectofinal/Objeto.java
@@ -9,6 +9,50 @@ package proyectofinal;
  *
  * @author Hachi
  */
-public class Objeto {
+public class Objeto 
+{
+    String categoria;
+    int codigo;
+    String nombre;
+    int precio;
+    
+    public Objeto(String categoria, int codigo, String nombre, int precio) {
+        this.categoria = categoria;
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+    
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
     
 }
