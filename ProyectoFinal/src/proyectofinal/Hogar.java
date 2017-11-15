cddbf90fa7f263482ba94e6221448257ef15648a
Chazito <f.nunez.acevedo@gmail.com>
2017-11-15 20:24:22 -0300

    Class

--- ProyectoFinal/src/proyectofinal/Hogar.java
+++ ProyectoFinal/src/proyectofinal/Hogar.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package proyectofinal;
 
+package proyectofinal;
+import java.util.ArrayList;
 /**
  *
  * @author Franco Nuñez
  */
 public class Hogar {
+    
+    ArrayList<Objeto> bañoLista;
+    ArrayList<Objeto> cocinaLista;
+    ArrayList<Objeto> iluminacionLista;
+    ArrayList<Objeto> menajeLista;
+    ArrayList<Objeto> mueblesLista;
+    ArrayList<Objeto> decoracionLista;
+    ArrayList<Objeto> textilLista;
+
+    public Hogar() {
+        this.bañoLista = new ArrayList<>();
+        this.cocinaLista = new ArrayList<>();
+        this.iluminacionLista = new ArrayList<>();
+        this.menajeLista = new ArrayList<>();
+        this.mueblesLista = new ArrayList<>();
+        this.decoracionLista = new ArrayList<>();
+        this.textilLista = new ArrayList<>();
+    }
+    
+    
     
 }
