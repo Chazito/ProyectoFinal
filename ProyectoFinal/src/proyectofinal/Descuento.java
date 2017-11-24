37bd6204bdba00eb875db6c602ff59b570a37de8
HokeRompeorli <Hamechrs@gmail.com>
2017-11-23 23:52:57 -0300

    Clase Descuento

---
+++ ProyectoFinal/src/proyectofinal/Descuento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinal;
+
+/**
+ *
+ * @author Hachi
+ */
+public class Descuento 
+{
+    
+    //METODO PARA APLICAR DESCUENTO EN EL PAGO
+    public void aplicarDescuento()
+    {
+        //Tomar forma de pago.
+        //Aplicar el descuento correspondiente a cada uno de los productos.
+        //Generar el producto con el nuevo precio y agregar a la lista nueva.
+    }       
+}
